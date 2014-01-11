984c15e58db32397638b085def5af1cb630bb56e
Ahmed <ahmed1490@gmail.com>
2014-01-11 15:15:32 +0530

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tripley::Application.config.secret_key_base = '13aa54c826978d48ded8e0d4572648ce85fe65beace1c604b72bb612d7becd36540b9296ce7254d0ab8447b743010bb99bbfb14e71fbd60b143648139a04b4e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tripley::Application.config.session_store :cookie_store, key: '_tripley_session'
